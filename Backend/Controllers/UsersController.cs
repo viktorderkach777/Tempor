7d6186b2255b72981d94dcf195f659687a269290
novakvova <novakvova@gmail.com>
2018-11-22 19:13:09 +0200

    Backend begin

---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using Backend.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Backend.Controllers
+{
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+    public class UsersController : ApiController
+    {
+        public SignupResultModel Post(SignupBindingModel model)
+        {
+            ErrorsSignupBindingModel errors = new ErrorsSignupBindingModel();
+            if(string.IsNullOrEmpty(model.UserName))
+                errors.username = "This field is required";
+
+            if (string.IsNullOrEmpty(model.Email))
+                errors.email = "This field is required";
+            try
+            {
+                MailAddress emailAddress = new MailAddress(model.Email);
+            }
+            catch
+            {
+                errors.email = "Email is invalid";
+            }
+            if (string.IsNullOrEmpty(model.Password))
+                errors.password = "This field is required";
+            if (!model.Password.Equals(model.PasswordConfirmation))
+                errors.passwordConfirmation = "Passwords must match";
+            if (string.IsNullOrEmpty(model.Timezone))
+                errors.timezone = "This field is required";
+            SignupResultModel result = new SignupResultModel
+            {
+                errors = errors,
+                isValid = false
+            };
+            if (!ModelState.IsValid)
+            {
+                return result;
+            }
+            return result;
+        }
+    }
+}
---
+++ Backend/Models/SignupBindingModels.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class SignupBindingModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Timezone")]
+        public string Timezone { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string PasswordConfirmation { get; set; }
+    }
+    public class SignupResultModel
+    {
+        public ErrorsSignupBindingModel errors { get; set; }
+        public bool isValid { get; set; }
+    }
+
+    public class ErrorsSignupBindingModel
+    {
+        public string email { get; set; }
+
+        public string username { get; set; }
+
+        public string timezone { get; set; }
+
+        public string password { get; set; }
+
+        public string passwordConfirmation { get; set; }
+    }
+}
